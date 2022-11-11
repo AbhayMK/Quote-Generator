913f257fda59901fe4443387ff6b2e85bf277ecc
AbhayMK <abhay.physics@gmail.com>
2022-11-11 09:04:28 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+let apiQuotes = [];
+
+// Get Quotes from API
+
+async function getQuotes() {
+  const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    console.log(apiQuotes);
+  } catch (error) {
+    //catch error here
+  }
+}
+
+//on Load
+getQuotes();
